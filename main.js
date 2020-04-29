01a46d525cde04457c19948e622b3709ae73e333
Erica Hill <acireh92@gmail.com>
2020-04-29 15:04:13 -0500

    First Application

---
+++ main.js
@@ -0,0 +1,5 @@
+//USe this line below to insert text or other things into your HTML page.You can simply copy and paste the small line below to add more items into your page.Be sure to add additional placeholders like "<p id= 'demo'> " and dont use the same id.
+document.getElementById("word").innerHTML = "Hello World !"
+
+// Shown below is a copy of the line above with a different id. Be sure to place a matching placeholder in your HTML page
+document.getElementById("word2").innerHTML = "Erica H"
